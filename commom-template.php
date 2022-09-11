76180bb7d719edddda67865df655878f3374f0ea
tawsifhye <tawsif.lucse@gmail.com>
2022-09-11 23:54:40 +0600

    WP Page Templates

---
+++ commom-template.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Template Name: Common Template
+*/
+get_header();
+
+if(have_posts()){
+    while(have_posts()){
+        the_post(); ?>
+<article class="page">
+    <div class="custom-page-layout">
+        <div class="page-title">
+        <h2><?php the_title();?></h2>
+        </div>
+        <div class="page-content">
+        <?php the_content(); ?>
+        </div>
+    </div>
+    
+  
+</article>
+
+   <?php }
+}
+
+else{
+  echo  '<h1>Hello World</h1>';
+}
+
+get_footer();
+
+?>
--- header.php
+++ header.php
@@ -4,6 +4,7 @@
         <meta charset="<?php bloginfo('charset');?>" >
         <meta name="viewport" content="width=device-width">
         <title><?php bloginfo('name');?></title>
+
         <?php wp_head();?>
     </head>
 
@@ -13,8 +14,15 @@
     <div class="custom-container">
     <header class="site-header">
         <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a></h1>
-        <h5><?php bloginfo('description'); ?></h5>
-
+        <h5>
+            <?php bloginfo('description'); ?>
+            <?php if(is_page(2)) { ?>
+        -Thank you for visiting our website
+        <?php } ?>
+    </h5>
+    
+        
+        
         <nav class="site-nav">
         <?php
         $args = array(
