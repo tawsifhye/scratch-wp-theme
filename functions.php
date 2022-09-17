776d311ff2a32c25808767aab39f452c47e63bc4
tawsifhye <tawsif.lucse@gmail.com>
2022-09-18 02:02:36 +0600

    WordPress Child Page Menu

--- functions.php
+++ functions.php
@@ -11,4 +11,34 @@ register_nav_menus(array(
     'footer' => __('Footer Menu'),
 ));
 
+//Get top ancestor
+function get_top_ancestor_id(){
+    global $post;
+    // echo'<pre>';
+    // print_r($post);
+    // echo'</pre>';
+    if($post->post_parent){
+        $ancestors = array_reverse(get_post_ancestors($post->ID));
+        return $ancestors[0];
+        
+    }
+ 
+     return $post->ID;
+}
+
+
+//Does Page have Children
+
+function has_children(){
+    global $post;
+
+   $pages = get_pages('child_of='.$post->ID);
+    echo'<pre>';
+    print_r(get_pages('child_of='.$post->ID));
+    echo'</pre>';
+   
+   return count($pages);
+}
+
+
 ?>
--- page.php
+++ page.php
@@ -6,6 +6,21 @@ if(have_posts()){
     while(have_posts()){
         the_post(); ?>
 <article class="page">
+  
+  <?php if(has_children() OR $post->post_parent > 0) { ?>
+    <span class="parent-link"><a href=<?php echo get_the_permalink(get_top_ancestor_id())?>><?php echo get_the_title(get_top_ancestor_id()) ?></a></span>
+<?php 
+  $args = array (
+    'child_of'=>get_top_ancestor_id(),
+    'title_li'=> ''
+  );
+
+  ?>
+  <?php wp_list_pages($args)?>
+   <?php } ?> 
+
+
+  
     <h2><?php the_title();?></h2>
     <?php the_content(); ?>
 </article>
