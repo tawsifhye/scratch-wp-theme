7051e024c7bd47a934f8fd19487d663f56cfe9e1
tawsifhye <tawsif.lucse@gmail.com>
2022-09-18 17:04:22 +0600

    post meta data

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ if(have_posts()){
         the_post(); ?>
 <article class="post">
     <h2><a href="<?php the_permalink();?>"> <?php the_title();?></a></h2>
+    <p><a href=<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>><?php echo the_author(); ?></a></p>
+    <p><?php echo the_time('F-j Y g:i a'); ?></p>
        <?php the_content(); ?>
 </article>
 
